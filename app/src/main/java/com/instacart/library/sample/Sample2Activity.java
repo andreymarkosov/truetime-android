471b762c8ec8bd00b9bcb2544de6ccacf646c7ef
Andrey Markosov <andrey.markosov@gmail.com>
2017-03-21 12:45:34 +0300

    button blinking added

--- app/src/main/java/com/instacart/library/sample/Sample2Activity.java
+++ app/src/main/java/com/instacart/library/sample/Sample2Activity.java
@@ -1,6 +1,8 @@
 package com.instacart.library.sample;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Button;
@@ -21,9 +23,12 @@ import rx.functions.Action1;
 import rx.schedulers.Schedulers;
 
 public class Sample2Activity
-      extends AppCompatActivity {
+        extends AppCompatActivity {
 
     private static final String TAG = Sample2Activity.class.getSimpleName();
+    private static final Handler handler = new Handler();
+    private CountDownTimer timer;
+    private int tickCount;
 
     @Bind(R.id.tt_btn_refresh) Button refreshBtn;
     @Bind(R.id.tt_time_gmt) TextView timeGMT;
@@ -43,29 +48,29 @@ public class Sample2Activity
         //TrueTimeRx.clearCachedInfo(this);
 
         TrueTimeRx.build()
-              .withConnectionTimeout(31_428)
-              .withRetryCount(100)
-              .withSharedPreferences(this)
-              .withLoggingEnabled(true)
-              .initializeRx("time.google.com")
-              .subscribeOn(Schedulers.io())
-              .observeOn(AndroidSchedulers.mainThread())
-              .subscribe(new Action1<Date>() {
-                  @Override
-                  public void call(Date date) {
-                      onBtnRefresh();
-                  }
-              }, new Action1<Throwable>() {
-                  @Override
-                  public void call(Throwable throwable) {
-                      Log.e(TAG, "something went wrong when trying to initializeRx TrueTime", throwable);
-                  }
-              }, new Action0() {
-                  @Override
-                  public void call() {
-                      refreshBtn.setEnabled(true);
-                  }
-              });
+                .withConnectionTimeout(31_428)
+                .withRetryCount(100)
+                .withSharedPreferences(this)
+                .withLoggingEnabled(true)
+                .initializeRx("time.google.com")
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action1<Date>() {
+                    @Override
+                    public void call(Date date) {
+                        onBtnRefresh();
+                    }
+                }, new Action1<Throwable>() {
+                    @Override
+                    public void call(Throwable throwable) {
+                        Log.e(TAG, "something went wrong when trying to initializeRx TrueTime", throwable);
+                    }
+                }, new Action0() {
+                    @Override
+                    public void call() {
+                        refreshBtn.setEnabled(true);
+                    }
+                });
     }
 
     @OnClick(R.id.tt_btn_refresh)
@@ -75,23 +80,49 @@ public class Sample2Activity
             return;
         }
 
-        Date trueTime = TrueTimeRx.now();
+        final Date trueTime = TrueTimeRx.now();
         Date deviceTime = new Date();
 
         Log.d("kg",
-              String.format(" [trueTime: %d] [devicetime: %d] [drift_sec: %f]",
-                            trueTime.getTime(),
-                            deviceTime.getTime(),
-                            (trueTime.getTime() - deviceTime.getTime()) / 1000F));
+                String.format(" [trueTime: %d] [devicetime: %d] [drift_sec: %f]",
+                        trueTime.getTime(),
+                        deviceTime.getTime(),
+                        (trueTime.getTime() - deviceTime.getTime()) / 1000F));
 
         timeGMT.setText(getString(R.string.tt_time_gmt,
-                                  _formatDate(trueTime, "yyyy-MM-dd HH:mm:ss", TimeZone.getTimeZone("GMT"))));
+                _formatDate(trueTime, "yyyy-MM-dd HH:mm:ss.SSS", TimeZone.getTimeZone("GMT"))));
         timePST.setText(getString(R.string.tt_time_pst,
-                                  _formatDate(trueTime, "yyyy-MM-dd HH:mm:ss", TimeZone.getTimeZone("GMT-07:00"))));
+                _formatDate(trueTime, "yyyy-MM-dd HH:mm:ss.SSS", TimeZone.getTimeZone("GMT-07:00"))));
         timeDeviceTime.setText(getString(R.string.tt_time_device,
-                                         _formatDate(deviceTime,
-                                                     "yyyy-MM-dd HH:mm:ss",
-                                                     TimeZone.getTimeZone("GMT-07:00"))));
+                _formatDate(deviceTime,
+                        "yyyy-MM-dd HH:mm:ss.SSS",
+                        TimeZone.getTimeZone("GMT-07:00"))));
+
+        // Blinking
+
+        long startDelay = 1000 - (trueTime.getTime() % 1000);
+        Log.i("delay", "startDelay = " + startDelay);
+
+        if (timer != null) {
+            timer.cancel();
+        }
+        handler.removeCallbacksAndMessages(null);
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Log.i("timer", "Start Timer : " + trueTime.getTime());
+                timer = new CountDownTimer(600000, 1000) {
+
+                    public void onTick(long millisUntilFinished) {
+                        Log.i("timer", "tick " + tickCount);
+                        refreshBtn.setBackgroundColor((tickCount % 2 == 0) ? 0xFF00FF88 : 0xFFFF0088);
+                        tickCount++;
+                    }
+
+                    public void onFinish() { }
+                }.start();
+            }
+        }, startDelay);
     }
 
     private String _formatDate(Date date, String pattern, TimeZone timeZone) {
